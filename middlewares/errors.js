9e0f5583d5425f76418f5c4f6049baa3331403f4
pranata.df <pranata.df@pranatadf.local>
2023-02-01 14:26:05 +0700

    add user controller

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -8,4 +8,9 @@ function errorHandler(err, req, res, next) {
     if (typeof err === "UnauthorizedError") {
         return res.status(401).json({ message: err.message });
     }
+    return res.status(500).json({message: err.message});
+}
+
+module.exports = {
+    errorHandler
 }
