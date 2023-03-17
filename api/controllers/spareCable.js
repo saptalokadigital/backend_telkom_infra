080847a8a8ff9a71518b2c3b270c0a2f9cb32998
ramahartadi <ramahartadi51@gmail.com>
2023-03-17 20:34:56 +0700

    add caable with auto

--- api/controllers/spareCable.js
+++ api/controllers/spareCable.js
@@ -2,116 +2,165 @@ const spareCableModel = require("../models/spare_cable.models");
 require("dotenv").config();
 
 exports.postSpareCable = async (req, res, next) => {
-  let data = new spareCableModel(req.body);
-  const result = await data.save();
-  res.send(result);
+    let data = new spareCableModel(req.body);
+    const result = await data.save();
+    res.send(result);
 };
 
 exports.deleteSpareCable = async (req, res, next) => {
-  console.log(req.params);
-  let data = await spareCableModel.deleteOne(req.params);
+    console.log(req.params);
+    let data = await spareCableModel.deleteOne(req.params);
 
-  res.send(data);
+    res.send(data);
 };
 
 exports.editSpareCable = async (req, res, next) => {
-  console.log(req.params);
-  let data = await spareCableModel.updateOne(req.params, { $set: req.body });
-  res.send(data);
+    console.log(req.params);
+    let data = await spareCableModel.updateOne(req.params, { $set: req.body });
+    res.send(data);
 };
 
 exports.getAll = (req, res, next) => {
-  spareCableModel.find({}).exec(function (err, cables) {
-    if (err) {
-      res.send("error has occured");
-    } else {
-      console.log(cables);
-      res.json(cables);
-    }
-  });
+    spareCableModel.find({}).exec(function (err, cables) {
+        if (err) {
+            res.send("error has occured");
+        } else {
+            console.log(cables);
+            res.json(cables);
+        }
+    });
 };
 
 exports.getPage = (req, res, next) => {
-  const page = parseInt(req.params.page, 10);
-  spareCableModel
-    .find({})
-    .skip((page - 1) * 20)
-    .limit(20)
-    .exec(function (err, cables) {
-      if (err) {
-        res.send("error has occured");
-      } else {
-        console.log(cables);
-        res.json(cables);
-      }
-    });
+    const page = parseInt(req.params.page, 10);
+    spareCableModel
+        .find({})
+        .skip((page - 1) * 20)
+        .limit(20)
+        .exec(function (err, cables) {
+            if (err) {
+                res.send("error has occured");
+            } else {
+                console.log(cables);
+                res.json(cables);
+            }
+        });
 };
 
 exports.getCablePopulate = (req, res, next) => {
-  spareCableModel
-    .aggregate([
-      {
-        $lookup: {
-          from: "systems",
-          localField: "system",
-          foreignField: "_id",
-          as: "system",
-        },
-      },
-      {
-        $unwind: "$system",
-      },
-      {
-        $lookup: {
-          from: "cable_types",
-          localField: "cable_type",
-          foreignField: "_id",
-          as: "cable_type",
-        },
-      },
-      {
-        $unwind: "$cable_type",
-      },
-      {
-        $lookup: {
-          from: "armoring_types",
-          localField: "armoring_type",
-          foreignField: "_id",
-          as: "armoring_type",
-        },
-      },
-      {
-        $unwind: "$armoring_type",
-      },
-      {
-        $lookup: {
-          from: "core_types",
-          localField: "core_type",
-          foreignField: "_id",
-          as: "core_type",
-        },
-      },
-      {
-        $unwind: "$core_type",
-      },
-      {
-        $lookup: {
-          from: "manufacturers",
-          localField: "manufacturer",
-          foreignField: "_id",
-          as: "manufacturer",
-        },
-      },
-      {
-        $unwind: "$manufacturer",
-      },
-    ])
-    .exec(function (err, cables) {
-      if (err) {
-        res.send("error has occured");
-      } else {
-        console.log(cables);
-        res.json(cables);
-      }
+    spareCableModel
+        .aggregate([
+            {
+                $lookup: {
+                    from: "systems",
+                    localField: "system",
+                    foreignField: "_id",
+                    as: "system",
+                },
+            },
+            {
+                $unwind: "$system",
+            },
+            {
+                $lookup: {
+                    from: "cable_types",
+                    localField: "cable_type",
+                    foreignField: "_id",
+                    as: "cable_type",
+                },
+            },
+            {
+                $unwind: "$cable_type",
+            },
+            {
+                $lookup: {
+                    from: "armoring_types",
+                    localField: "armoring_type",
+                    foreignField: "_id",
+                    as: "armoring_type",
+                },
+            },
+            {
+                $unwind: "$armoring_type",
+            },
+            {
+                $lookup: {
+                    from: "core_types",
+                    localField: "core_type",
+                    foreignField: "_id",
+                    as: "core_type",
+                },
+            },
+            {
+                $unwind: "$core_type",
+            },
+            {
+                $lookup: {
+                    from: "manufacturers",
+                    localField: "manufacturer",
+                    foreignField: "_id",
+                    as: "manufacturer",
+                },
+            },
+            {
+                $unwind: "$manufacturer",
+            },
+        ])
+        .exec(function (err, cables) {
+            if (err) {
+                res.send("error has occured");
+            } else {
+                console.log(cables);
+                res.json(cables);
+            }
+        });
+};
+
+exports.createSpareCable = async (req, res, next) => {
+    const tank_inner = req.body.tank_inner;
+    const tank_outer = req.body.tank_outer;
+    let highest_tank = 0;
+    // should only one of them be filled
+    if (tank_inner && tank_outer) {
+        res.status(400).send(
+            "Only one of tank_inner or tank_outer should be filled"
+        );
+        return;
+    }
+    // should be one of them filled
+    if (!tank_inner && !tank_outer) {
+        res.status(400).send(
+            "One of tank_inner or tank_outer should be filled"
+        );
+        return;
+    }
+    //check if tank_inner or tank_outer is filled
+    //if tank_inner is filled
+    //find the highest tank_inner
+    if (tank_inner) {
+        const highest_tank_inner = await spareCableModel
+            .find({ tank_inner: tank_inner })
+            .sort({ tank_level: -1 })
+            .limit(1);
+        if (highest_tank_inner.length > 0) {
+            highest_tank = highest_tank_inner[0].tank_level;
+        }
+    }
+    //if tank_outer is filled
+    //find the highest tank_outer
+    if (tank_outer) {
+        const highest_tank_outer = await spareCableModel
+            .find({ tank_outer: tank_outer })
+            .sort({ tank_level: -1 })
+            .limit(1);
+        if (highest_tank_outer.length > 0) {
+            highest_tank = highest_tank_outer[0].tank_level;
+        }
+    }
+    let data = new spareCableModel({
+        ...req.body,
+        tank_level: highest_tank + 1,
     });
+    const result = await data.save();
+    res.send(result);
 };
--- api/routes/spareCable.js
+++ api/routes/spareCable.js
@@ -6,7 +6,7 @@ router.get("/data/", spareCableController.getAll);
 router.get("/data/:page", spareCableController.getPage);
 router.get("/", spareCableController.getCablePopulate);
 
-router.post("/", spareCableController.postSpareCable);
+router.post("/", spareCableController.createSpareCable);
 router.delete("/:_id", spareCableController.deleteSpareCable);
 router.put("/:_id", spareCableController.editSpareCable);
 module.exports = router;
