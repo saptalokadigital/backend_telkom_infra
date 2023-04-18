63ea5760a6651aa6ba53162a3a14912b9681a794
ramahartadi <ramahartadi51@gmail.com>
2023-04-18 14:42:39 +0700

    update dashboard

--- api/controllers/dashboard.js
+++ api/controllers/dashboard.js
@@ -56,3 +56,61 @@ exports.getChart = async (req, res, next) => {
       }
     });
 };
+
+// get chart by cable type and armoring type
+exports.getChartByCableTypeAndArmoringType = async (req, res, next) => {
+  spareCableModel
+    .aggregate([
+      //Group by cable type and armoring type
+      {
+        $group: {
+          _id: {
+            cable_type: "$cable_type",
+            armoring_type: "$armoring_type",
+          },
+          length: { $sum: "$length_report" },
+          totalCable: { $sum: 1 },
+        },
+      },
+      {
+        $lookup: {
+          from: "cable_types",
+          localField: "_id.cable_type",
+          foreignField: "_id",
+          as: "cable_type",
+        },
+      },
+      {
+        $unwind: "$cable_type",
+      },
+      {
+        $lookup: {
+          from: "armoring_types",
+          localField: "_id.armoring_type",
+          foreignField: "_id",
+          as: "armoring_type",
+        },
+      },
+      {
+        $unwind: "$armoring_type",
+      },
+      {
+        $project: {
+          _id: 0,
+          cable_type: "$cable_type.cable_type",
+          armoring_type: "$armoring_type.armoring_type",
+          length: 1,
+          totalCable: 1,
+        },
+      },
+    ])
+    .exec(function (err, result) {
+      if (err) throw err;
+      const filteredResult = result.filter(
+        (group) =>
+          group.cable_type === req.params.cable_type &&
+          group.armoring_type === req.params.armoring_type
+      );
+      res.json(filteredResult);
+    });
+};
--- api/routes/dashboard.js
+++ api/routes/dashboard.js
@@ -4,5 +4,9 @@ const dashboardController = require("../controllers/dashboard");
 const auth = require("../middleware/auth");
 
 router.get("/chart", dashboardController.getChart);
+router.get(
+  "/chart/:cable_type/:armoring_type",
+  dashboardController.getChartByCableTypeAndArmoringType
+);
 // router.get("/spare_kits", dashboardController.getdashboardKitsPopulate);
 module.exports = router;
