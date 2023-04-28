a93dd253f50253cf924ed2ef72c1c1607864d8aa
ramahartadi <ramahartadi51@gmail.com>
2023-04-28 15:05:45 +0700

    add dev and start, get chart grouped by cable_type

--- api/controllers/dashboard.js
+++ api/controllers/dashboard.js
@@ -114,3 +114,70 @@ exports.getChartByCableTypeAndArmoringType = async (req, res, next) => {
       res.json(filteredResult);
     });
 };
+
+exports.getChartByCableType = async (req, res, next) => {
+  spareCableModel
+    .aggregate([
+      //Group by cable type and armoring type
+      {
+        $group: {
+          _id: {
+            cable_type: "$cable_type",
+            armoring_type: "$armoring_type",
+          },
+          length: { $sum: "$length_report" },
+          totalCable: { $sum: 1 },
+        },
+      },
+      {
+        $lookup: {
+          from: "cable_types",
+          localField: "_id.cable_type",
+          foreignField: "_id",
+          as: "cable_type",
+        },
+      },
+      {
+        $unwind: "$cable_type",
+      },
+      {
+        $lookup: {
+          from: "armoring_types",
+          localField: "_id.armoring_type",
+          foreignField: "_id",
+          as: "armoring_type",
+        },
+      },
+      {
+        $unwind: "$armoring_type",
+      },
+      // Group by cable type
+      {
+        $group: {
+          _id: "$cable_type.cable_type",
+          armoring_types: {
+            $push: {
+              armoring_type: "$armoring_type.armoring_type",
+              length: "$length",
+              totalCable: "$totalCable",
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          cable_type: "$_id",
+          armoring_types: 1,
+        },
+      },
+    ])
+    .exec(function (err, cables) {
+      if (err) {
+        res.send("error has occured");
+      } else {
+        console.log(cables);
+        res.json(cables);
+      }
+    });
+};
