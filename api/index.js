458213c9fcf5be7e4b0f123e3e2d3db91f38a141
ramahartadi <ramahartadi51@gmail.com>
2023-05-27 17:02:29 +0700

    Merapikan kodingan

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+const port = process.env.PORT || 1000;
+const connectToDatabase = require("./database");
+
+const cors = require("cors");
+const logger = require("./logger");
+
+async function startServer() {
+  await connectToDatabase();
+
+  app.use(express.json());
+  app.use(cors());
+  app.use(morgan("dev"));
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+
+  app.use("/api", routes);
+
+  app.use((err, req, res, next) => {
+    logger.error(err.stack);
+    res.status(err.statusCode || 500).send({ error: err.message });
+  });
+
+  app.listen(port, () => {
+    logger.info(`Server running at http://localhost:${port}/`);
+  });
+}
+module.exports = startServer;
